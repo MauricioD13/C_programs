fb97713905a3238ac74237d3b4a2d798291ecade
Mauricio David <mdavid_cuello@javeriana.edu.co>
2020-08-06 21:06:14 -0500

    Vamos a enviar un .cpp de prueba

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Prueba
 
 Una prueba del uso de gitlab
+Algunos programas para poder recordar la programacion en C
---
+++ prueba.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main(){
+    string nombre;
+    cout<<"prueba de la salida \n ingrese su nombre: "<<endl;
+    cin>>nombre;
+}
