9a82dbf6d9b34aafcf31f306bcc268168b419d12
Mauricio David <mdavid_cuello@javeriana.edu.co>
2020-08-10 21:48:52 -0500

    Prueba C++

--- prueba.cpp
+++ prueba.cpp
@@ -2,9 +2,20 @@
 #include <iostream>
 #include <string.h>
 using namespace std;
-
+void agregar_valor(int *A,int *B,int *resultado){
+    *resultado=*A+*B;
+}
 int main(){
-    string nombre;
-    cout<<"prueba de la salida \n ingrese su nombre: "<<endl;
-    cin>>nombre;
+    int datoA=4;
+    int datoB=5;
+    int res;
+    int *apuntadorA=&datoA;
+    int *apuntadorB=&datoB;
+    int *resultado=&res;
+    agregar_valor(apuntadorA,apuntadorB,resultado);
+    cout<<"Resultado: "<<*resultado;  
+    /*for(int i=0;i<5;i++){
+        apuntador=&datos[i];
+        cout<<"Apuntador: "<<apuntador<<"\t Valor: "<<*apuntador<<endl;
+    }*/
 }
