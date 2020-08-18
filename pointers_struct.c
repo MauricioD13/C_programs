c4144b673c96d144cd3b55e5daf4253723065f50
Mauricio David <mdavid_cuello@javeriana.edu.co>
2020-08-18 15:49:37 -0500

    Ejercicio de apuntadores con estructuras

---
+++ pointers_struct.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Date{
+    int *day;
+    int *month;
+    int *year;
+};
+int main(){
+    struct Date *first;
+    first=(struct Date *)malloc(sizeof(struct Date));
+    first->day=(int *)malloc(sizeof(int));
+    first->month=(int *)malloc(sizeof(int));
+    first->year=(int *)malloc(sizeof(int));
+    *(first->day)=5;
+    *(first->month)=12;
+    *(first->year)=1998;
+    printf("dia: %d, mes: %d, año: %d \n", *(first->day),*(first->month),*(first->year));
+    free(first->day);
+    free(first->month);
+    free(first->year);
+    free(first);
+}
