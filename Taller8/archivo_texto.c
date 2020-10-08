b178a281058380f488f172cf96e0a32e810e14c4
Mauricio David <mdavid_cuello@javeriana.edu.co>
2020-10-08 13:45:42 -0500

    Taller 8 completo, escritura y lectura de archivos de texto

--- Taller8/archivo_texto.c
+++ Taller8/archivo_texto.c
@@ -1,22 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+/*Nombre: Mauricio David Cuello Alzate
+  Carrera: Ing Electronica
+  compilador: gcc en terminal
+  Nota: El archivo de entrada esta separado por ";"
+*/
 typedef struct REGISTRO {
-    int ID;
+    int *ID;
     char *nombre;
-    float nota;
+    float *nota;
 }REGISTRO;
+
+
+
 void llenar(REGISTRO *entrada,char *linea){
-    char *token=malloc(sizeof(linea));
-    token=strtok(linea,";");
-    printf("TOKEN: %s\n",token);
-    entrada->ID=atoi(token);
+    char *token=malloc(10*sizeof(linea));
+    char *copy=malloc(10*sizeof(linea));
+    int *ID=malloc(sizeof(int));
+    char *nombre=malloc(sizeof(char *));
+    float *nota=malloc(sizeof(float));
+    strcpy(copy,linea);
+    token=strtok(copy,";");
+    *ID=atoi(token);
+    entrada->ID=ID;
+
+
     token=strtok(NULL,";");
-    entrada->nombre=token;
+    nombre=token;
+    entrada->nombre=nombre;
+
+
     token=strtok(NULL,";");
-    entrada->nota=atof(token);
+    *nota=atof(token);
+    entrada->nota=nota;
+   
 }
+
+
 
 void print_registros(REGISTRO **tabla_registros,int cont){
 
@@ -24,7 +45,72 @@ void print_registros(REGISTRO **tabla_registros,int cont){
         printf("\n Persona: %d \n ID:%d\n Nombre: %s\n nota:%3f\n",i,(*(tabla_registros+i))->ID,(*(tabla_registros+i))->nombre,(*(tabla_registros+i))->nota);
     }
 }
+void print_strings(char **array, size_t len) 
+{ 
+    size_t i;
+ 
+    for(i=0; i<len; i++){ 
+        printf("%s|", array[i]);
+    }
+    printf("\n");
+ 
+} 
+int comparar(const void *a, const void *b) 
+{ 
+    const char **ia = (const char **)a;
+    const char **ib = (const char **)b;
+    return strcmp(*ia, *ib);
+} 
+char* evaluar(float x){
+    char *result;
+    if(x<3){
+        result="C";
+    }else if(3<x && x<4){
+        result="B";
+    }else if(4<=x){
+        result="A";
+    }
+    return result;
 
+}
+void imprimir(char *strings[3],REGISTRO **tabla_registros){
+    FILE *nuevo_archivo;
+    nuevo_archivo=fopen("sort_notes.txt","w");
+    for(int j=0;j<3;j++){
+        for(int i=0;i<3;i++){
+            int result = strcmp(((*(tabla_registros+i))->nombre),strings[j]);
+            if(!result){
+                char* printed=malloc(100*sizeof(char));
+                char* result=malloc(100*sizeof(char));
+                snprintf(result,10,"%d",*((*(tabla_registros+i))->ID));
+                printed=result;
+                strcat(printed,"\t");
+                strcat(printed,((*(tabla_registros+i))->nombre));
+                strcat(printed,"\t");
+
+                char buf[100]; 
+                
+                gcvt(*((*(tabla_registros+i))->nota),3, buf); 
+  
+
+                strcat(printed,buf);
+                strcat(printed,"\t");
+                char* eval;
+                eval=evaluar(*((*(tabla_registros+i))->nota));
+                strcat(printed,eval);
+                strcat(printed,"\n");
+                int status=fputs(printed,nuevo_archivo);
+            } 
+            
+        }
+    }
+    fclose(nuevo_archivo);
+
+}
+
+
+
+ 
 int main(){
     FILE *archivo;
     REGISTRO **tabla_registros=malloc(10*sizeof(REGISTRO*));
@@ -44,8 +130,23 @@ int main(){
         fgets(linea,1000,archivo);
         llenar(registro,linea);
         *(tabla_registros+cont)=registro;
-        cont++;
+        cont+=1;
     }
-    print_registros(tabla_registros,cont);
+    //print_registros(tabla_registros,cont);
+    //printf("Nombre0:%s\n",((*(tabla_registros))->nombre)+1);
+    //printf("Nombre1:%s\n",((*(tabla_registros+2))->nombre)+1);
+    char *strings[3];
+    for(int i=0;i<3;i++){
+        char *copy=malloc(sizeof(char*));
+        char *token=malloc(sizeof(char*));
+        strings[i]=((*(tabla_registros+i))->nombre);
+    }
+    size_t strings_len = sizeof(strings) / sizeof(char *);
+    qsort(strings, strings_len, sizeof(char *),comparar);
+    
+    imprimir(strings,tabla_registros);
+    
+    
     fclose(archivo);
+    free(tabla_registros);
 }
