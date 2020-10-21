908add2042bf92d00b84e1c4c06857e708321422
Mauricio <mdavid_cuello@javeriana.edu.co>
2020-10-20 23:38:18 -0500

    Taller 11 85% falto funcion para agregar empleado

--- Taller11_binary_file/empresa.c
+++ Taller11_binary_file/empresa.c
@@ -5,10 +5,10 @@
 
 typedef struct EMPLEADO
 {
-    char *codigo;
-    char *apellido;
-    char *nombre;
-    char *celular;
+    int codigo;
+    char apellido[20];
+    char nombre[20];
+    int celular;
 }EMPLEADO;
 
 
@@ -36,57 +36,113 @@ int tokenized_lines(char *datos,char **lineas){
     
     return i;
 }
-void tokenized_datos(char *linea){
-    printf("Aqui voy\n");
-    char *token=malloc(sizeof(linea));
-    char *copy=malloc(sizeof(linea));
-    
-    EMPLEADO *empleado=malloc(sizeof(EMPLEADO));
-    strcpy(copy,linea);
-    
-    char *codigo=malloc(sizeof(linea));
-    char *apellido=malloc(sizeof(linea));
-    char *nombre=malloc(sizeof(linea));
-    char *celular=malloc(sizeof(linea));
-    
-    codigo=strtok(copy,",");
-    empleado->codigo=codigo;
-    apellido=strtok(NULL,",");
-    empleado->apellido=apellido;
-    nombre=strtok(NULL,",");
-    empleado->nombre=nombre;
-    celular=strtok(NULL,",");
-    empleado->celular=celular;
-    printf("Voy saliendo\n");
 
+void print_empleado(EMPLEADO *empleados,int indices[5]){
+    for(int j=0;j<5;j++){
+        printf("indices:%d\n",indices[j]);
+        printf("codigo:%d\n",(empleados+(indices[j]))->codigo);
+        //printf("longitud cod:%d\n",strlen((empleados+j)->codigo));
+        printf("apellido:%s\n",(empleados+(indices[j]))->apellido);
+        printf("nombre: %s\n",(empleados+(indices[j]))->nombre);
+        printf("celular:%d\n",(empleados+(indices[j]))->celular);
+        printf("-----------------------\n");
+    }
 }
 
-
-
-
+void escribirTexto(EMPLEADO *empleados,int indices[5]){
+    FILE *archivo_texto;
+    archivo_texto = fopen("datos.txt","w");
+    char *result=malloc(sizeof(EMPLEADO));
+    char *temp=malloc(sizeof(EMPLEADO));
+    for(int k=0;k<5;k++){
+        snprintf(result,10,"%d",(empleados+indices[k])->codigo);
+        strcat(result,",");
+        strcat(result,(empleados+indices[k])->apellido);
+        strcat(result,",");
+        strcat(result,(empleados+indices[k])->nombre);
+        snprintf(temp,10,"%d",(empleados+indices[k])->celular);
+        strcat(result,",");
+        strcat(result,temp);
+        strcat(result,"\n");
+        fputs(result,archivo_texto);
+    }
+    fclose(archivo_texto);
+}
+int menu(int option){
+    printf("Menu\n");
+    printf("[1] Imprimir empleados\n");
+    printf("[2] Agregar empleado\n"); // No funciono
+    printf("[3] Salir\n");
+    scanf("%d",&option);
+    return option;
+}
 int main(){
+
     FILE *datos_binarios;
     FILE *indice_binarios;
 
-    char *datos=malloc(100*sizeof(char*));
-    char *indices=malloc(100*sizeof(char*));
+    int option=0;
+    option=menu(option);
+    
+    int indices[5];
+    int *datos1=malloc(sizeof(int));
+    
+    char *datos2=malloc(20*sizeof(char*));
+    
     
     datos_binarios = fopen("datos.bin","rb");
     indice_binarios = fopen("indice.bin","rb");
-    
-    fread(datos,sizeof(char),200,datos_binarios);
-    fread(indices,sizeof(char),100,indice_binarios);
-    char **lineas=malloc(100*sizeof(datos));
-    printf("%s\n",datos);
-    printf("%s\n",indices);
-    int cont_lineas=0;
-    cont_lineas=tokenized_lines(datos,lineas);
 
-    for(int i = 0; i<cont_lineas; i++){
-        printf("lineas:%s\n",*(lineas+i));
+    int position;
+    fseek(datos_binarios,0,SEEK_END);
+    int x=ftell(datos_binarios);
+    fseek(datos_binarios,0,SEEK_SET);
+    
+    EMPLEADO *empleados=malloc(5*sizeof(EMPLEADO));
+    int i=0;
+    while(position<x){
+        EMPLEADO empleado;
+        fread(datos1,sizeof(int),1,datos_binarios);
+        empleado.codigo=*datos1;
+        fread(datos2,sizeof(char),21,datos_binarios);
+        strcpy(empleado.apellido,datos2);
+        fread(datos2,sizeof(char),21,datos_binarios);
+        strcpy(empleado.nombre,datos2);
+        fread(datos1,sizeof(int),1,datos_binarios);
+        empleado.celular=*datos1;
+        
+        position=ftell(datos_binarios);  
+        *(empleados+i)=empleado;
+        i++;
     }
 
-    tokenized_datos(*(lineas+1));
+    fseek(indice_binarios,0,SEEK_END);
+    x=ftell(indice_binarios);
+    
+    fseek(indice_binarios,0,SEEK_SET);
+    i=0;
+    position=0;
+    while(position<x){
+        fread(datos1,sizeof(int),1,indice_binarios);
+        indices[i]=*datos1;
+        position=ftell(indice_binarios);
+        i++;
+    }
+   
+    while(option!=3){
+        if(option==1){
+            print_empleado(empleados,indices);
+        }
+        else if(option==2){
+            printf("-----------------------\n");
+            printf("||                    ||\n");
+            printf("||     No funciono :( ||\n");
+            printf("||                    ||\n");
+            printf("-----------------------\n");
+        }
+        option=menu(option);
+    }
+    escribirTexto(empleados,indices);
 
     fclose(datos_binarios);
     fclose(indice_binarios);
--- Taller11_binary_file/make_binary.c
+++ Taller11_binary_file/make_binary.c
@@ -3,7 +3,12 @@
 #include <string.h>
 
 
-
+typedef struct PERSONA{
+    char codigo[4];
+    char apellido[20];
+    char nombre[20];
+    char celular[4];
+}PERSONA;
 
 
 int main()
@@ -12,11 +17,11 @@ int main()
     FILE *fp = NULL;
     FILE *file = NULL;
     //Message
-    char * codigo[5] ={"1021","1064","1052","1033","1005"};
-    char * apellido[5]={"Zapata Diaz","Sanchez Gomez","Correa Cruz","Arias Mendez","Duarte Garcia"};
-    char * nombre[5]={"Teresa","Elizabeth","Carolina","Adriana","Diana Maria"};
-    char * celular[5]={"3102","3164","3000","3174","3165"};
-    char * indices[6]={"5","4","2","1","3"};
+    int codigo[5] ={1021,1064,1052,1033,1005};
+    char *apellido[5]={"Zapata Diaz","Sanchez Gomez","Correa Cruz","Arias Mendez","Duarte Garcia"};
+    char *nombre[5]={"Teresa","Elizabeth","Carolina","Adriana","Diana Maria"};
+    int celular[5]={3102,3164,3000,3174,3165};
+    int indices[5]={3,2,4,1,0};
     
     //Variable for message length
     int length = 0;
@@ -28,26 +33,30 @@ int main()
         printf("Error in creating the file\n");
         exit(1);
     }
+
     
-    
+
     for(int i = 0;i<5;i++){
-        length = strlen(codigo[i]);
-        fwrite(codigo[i], sizeof(char),length, fp);
-        fwrite(",",sizeof(char),1,fp);
-        length = strlen(apellido[i]);
-        fwrite(apellido[i],sizeof(char),length,fp);
-        fwrite(",",sizeof(char),1,fp);
-        length = strlen(nombre[i]);
-        fwrite(nombre[i],sizeof(char),length,fp);
-        fwrite(",",sizeof(char),1,fp);
-        length = strlen(celular[i]);
-        fwrite(celular[i],sizeof(char),length,fp);
-        fwrite("\n",sizeof(char),1,fp);
-        fwrite(indices[i],sizeof(char),1,file);
-        fwrite("\n",sizeof(char),1,file);
+        
+        fwrite(&codigo[i],sizeof(int),1,fp);
+
+        length=(strlen(apellido[i]));
+        fwrite(apellido[i],length+1,1,fp);
+        fseek(fp,(20-length),SEEK_CUR);
+
+        length=(strlen(nombre[i]));
+        fwrite(nombre[i],length+1,1,fp);
+        fseek(fp,(20-length),SEEK_CUR);
+
+        fwrite(&celular[i],sizeof(int),1,fp);
+        
+
+        fwrite(&indices[i],sizeof(int),1,file);
         
     }
+
     fflush(fp);
     fclose(fp);
+    fclose(file);
     return 0;
 }
