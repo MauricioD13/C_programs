3413fc2312119185e73a158c8068b7e0707d677b
Mauricio David Cuello Alzate <mdavid_cuello@javeriana.edu.co>
2020-11-20 18:54:10 -0500

    Taller 14 100%

--- Taller14_stack_queue/impresoras.cpp
+++ Taller14_stack_queue/impresoras.cpp
@@ -10,9 +10,9 @@ typedef struct IMPRESORA{
 
 
 void mostrar_impresoras(IMPRESORA *impresoras){
-    
+
     for(int i=0;i<5;i++){
-        
+
         cout<<"Impresora "<<i<<"\tEstado: ";
         ((impresoras+i)->estado == 0) ? cout<<"apagado\t":cout<<"encendido";
         cout<<"\tTrabajos pendientes: "<<(impresoras+i)->trabajos_pendientes<<endl;
@@ -27,14 +27,21 @@ int menu(){
     cin>>option;
     return option;
 }
-void asignador(IMPRESORA *impresoras,queue <int> cola_impresiones){
+void asignador(IMPRESORA *impresoras,queue <int> *cola_impresiones,const int limite){
     int min;
     int index;
     int copias;
-    while(!cola_impresiones.empty()){
-        min = (impresoras)->trabajos_pendientes;
-        index = 0;
-        
+    while(!cola_impresiones->empty()){
+        int k=0;
+        while(1){
+            if((impresoras+k)->estado != 0){
+                  min = (impresoras + k)->trabajos_pendientes;
+                  index = k;
+                   break;
+            }
+            k++;
+        }
+
        for(int i=0;i<5;i++){
            if(((impresoras+i)->trabajos_pendientes == 0)&&((impresoras+i)->estado!=0)){
                index = i;
@@ -44,19 +51,22 @@ void asignador(IMPRESORA *impresoras,queue <int> cola_impresiones){
                index = i;
                min = (impresoras+i)->trabajos_pendientes;
            }
-           
-       }
-       
 
-        (impresoras+index)->trabajos_pendientes += cola_impresiones.front();
-        cola_impresiones.pop();
+       }
+        if(((impresoras+index)->trabajos_pendientes+cola_impresiones->front()) <= limite){
+            (impresoras+index)->trabajos_pendientes += cola_impresiones->front();
+            cola_impresiones->pop();
+        }else{
+            cout<<"Bloque de "<<cola_impresiones->front()<<" trabajos en cola han sido descartados"<<endl;
+            cola_impresiones->pop();
+        }
     }
 }
 
-void redistribucion(IMPRESORA *impresoras){
+void redistribucion(IMPRESORA *impresoras,const int limite){
         int cambio;
         cout<<"Cual impresora desea cambiar?"<<endl;
-        cin>>cambio; 
+        cin>>cambio;
         //Funcion de distribucion
         int min,max;
         int index;
@@ -68,34 +78,49 @@ void redistribucion(IMPRESORA *impresoras){
             cout<<"[Redistribuyendo trabajos...]"<<endl;
             cout<<"----------------------------------------\n";
             while(trabajos!=0){
-                min = (impresoras)->trabajos_pendientes;
-                index = 0;
-                
-                for(int i=0;i<5;i++){
-                    if(((impresoras+i)->trabajos_pendientes == 0)&&(i!=cambio)&&((impresoras+i)->estado!=0)){
-                        index = i;
+                int k=0;
+                while(1){
+                    if((impresoras+k)->estado != 0){
+                        min = (impresoras + k)->trabajos_pendientes;
+                        index = k;
                         break;
-                    }else if(((impresoras+i)->trabajos_pendientes <= min)&&(i!=0)&&(i!=cambio)&&((impresoras+i)->estado!=0))
-                    {
-                        index = i;
-                        min = (impresoras+i)->trabajos_pendientes;
                     }
-                    
+                    k++;
                 }
-            
 
-                (impresoras+index)->trabajos_pendientes +=1;
-                trabajos--;
-                
+                for(int i=0;i<5;i++){
+                    if(((impresoras+i)->trabajos_pendientes == 0)&&(i!=cambio)&&((impresoras+i)->estado!=0)){
+                        index = i;
+                        break;
+                    }else if(((impresoras+i)->trabajos_pendientes <= min)&&(i!=0)&&(i!=cambio)&&((impresoras+i)->estado!=0))
+                    {
+
+                        index = i;
+                        min = (impresoras+i)->trabajos_pendientes;
+                    }
+
+                }
+                if((((impresoras+index)->trabajos_pendientes)+1)<=limite){
+                    (impresoras+index)->trabajos_pendientes +=1;
+                    trabajos--;
+                }else{
+                    cout<<"Se han descartado "<<trabajos<<" en la redistribucion"<<endl;
+                    trabajos = 0;
+                }
+
             }
-        }else
-        {       
-                
-                
-            
-                min = (impresoras)->trabajos_pendientes;
-                index = 0;
-                
+        }else{
+                int k=0;
+                while((impresoras+k)->estado==0){
+                    min = (impresoras+k)->trabajos_pendientes;
+                    index = k;
+                }
+                if(k==0){
+                    min = (impresoras)->trabajos_pendientes;
+                    index = 0;
+                }
+                cout<<"minimo: "<<min<<endl;
+
                 for(int i=0;i<5;i++){
                     if(((impresoras+i)->trabajos_pendientes == 0)&&(i!=cambio)&&((impresoras+i)->estado!=0)){
                         break;
@@ -103,7 +128,7 @@ void redistribucion(IMPRESORA *impresoras){
                     {
                         min = (impresoras+i)->trabajos_pendientes;
                     }
-                    
+
                 }
                 cout<<"minimo: "<<min<<endl;
                 while(min>(impresoras+cambio)->trabajos_pendientes){
@@ -118,7 +143,7 @@ void redistribucion(IMPRESORA *impresoras){
                             index = i;
                             max = (impresoras+i)->trabajos_pendientes;
                         }
-                    
+
                     }
                     (impresoras+index)->trabajos_pendientes -=1;
                     (impresoras+cambio)->trabajos_pendientes +=1;
@@ -131,7 +156,7 @@ int main(){
     queue <int> cola_impresiones;
     int option=0;
     int numero;
-    
+
     IMPRESORA *impresoras = new IMPRESORA[5];
     for(int i=0;i<5;i++){
         IMPRESORA impresora;
@@ -144,35 +169,25 @@ int main(){
         cout<<"Cola "<<j<<" :\t"<<numero<<endl;
         cola_impresiones.push(numero);
     }
-    const int limite_trabajos=10;
+    const int limite_trabajos=15;
     cout<<"----------------------------------------\n";
-    /*while (option!=2)
-    {
-        cout<<"Valor: "<<endl;
-        cin>>numero;
-        cola_impresiones.push(numero);
-        cout<<"Desea agregar otro valor?"<<endl;
-        cout<<"[1] Si [2] No"<<endl;
-        cin>>option;
-    }*/
-    asignador(impresoras,cola_impresiones);
+
+    asignador(impresoras,&cola_impresiones,limite_trabajos);
     cout<<"----------------------------------------"<<endl;
-    
-    
+
+
     mostrar_impresoras(impresoras);
-    
+
     //option = menu();
     option = 1;
-    
+
     while(option!=0){
     cout<<"----------------------------------------\n";
     option = menu();
     switch(option){
         case 1:
-            redistribucion(impresoras);
+            redistribucion(impresoras,limite_trabajos);
             mostrar_impresoras(impresoras);
-            cout<<"Otro cambio\t";
-            cin>>option;
             break;
         case 2:
             int j=1;
@@ -185,13 +200,13 @@ int main(){
                 cin>>j;
             }
             mostrar_impresoras(impresoras);
-            asignador(impresoras,cola_impresiones);
+            asignador(impresoras,&cola_impresiones,limite_trabajos);
             cout<<"----------------------------------------\n";
             mostrar_impresoras(impresoras);
 
-    
-    
-    
+
+
+
         }
 
     }
